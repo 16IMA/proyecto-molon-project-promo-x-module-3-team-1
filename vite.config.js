f00d7a7689c712891e58495a29a060c31e3d48af
16IMA <imonfort001@gmail.com>
2025-02-19 21:50:14 +0100

    docs 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/proyecto-molon-project-promo-x-module-3-team-1/',
   plugins: [react()],
   server: {
     open: "/index.html",
