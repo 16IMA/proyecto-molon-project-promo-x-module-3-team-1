781efb8a3c14d25b4a20984b512230b1f9f53e99
16IMA <imonfort001@gmail.com>
2025-02-20 18:52:50 +0100

    change error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -60,7 +60,7 @@ function App() {
     fetch('https://dev.adalab.es/api/projectCard', {
       method: 'POST',
       headers: {'Content-Type':'application/json'},
-      body:JSON.stringify(FormData)
+      body:JSON.stringify(formData)
     })
     .then( response => response.json())
     .then( responseData => {
